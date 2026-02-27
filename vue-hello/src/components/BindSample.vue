7b95d4d3470a439edbf8bb0ae7a646a9acd5ceff
kimeunji806 <sacri8fice@gmail.com>
2026-02-27 17:46:00 +0900

    260227 수업

--- vue-hello/src/components/BindSample.vue
+++ vue-hello/src/components/BindSample.vue
@@ -7,5 +7,6 @@ const url = ref("http://vuejs.org/");
 
 <template>
   <p><a v-bind:href="url" target="_blank">Vue.js 사이트</a></p>
-  <p><a>Vue.js 사이트(약어)</a></p>
+  <p><a :href="url" target="_blank">Vue.js 사이트(약어)</a></p>
+  <p><a v-bind:href="url + 'guide/introduction.html'" target="_blank">Vue.js 가이드 페이지</a></p>
 </template>
